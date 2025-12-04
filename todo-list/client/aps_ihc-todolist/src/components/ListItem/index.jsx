24eaeb6f1744c62605567f2edce8e6a897bea852
Nic-Rocha <nicolle.rocha.araujo@gmail.com>
2025-12-04 16:14:25 -0300

    fix(front): Modificação do front do listpage

--- todo-list/client/aps_ihc-todolist/src/components/ListItem/index.jsx
+++ todo-list/client/aps_ihc-todolist/src/components/ListItem/index.jsx
@@ -1,22 +1,32 @@
 import './style.css'
 
-function ListItem({ nome, quantidade, valor, data }) {
+function ListItem({ nome, quantidade, valor, data, concluido, onToggle }) {
   return (
-    <li>
+    <li className={concluido ? 'item-concluido' : ''}>
       <div className="list-item-container">
         <table className="table-item">
           <tbody>
             <tr>
-              <th>{nome}</th>
-              <th>{quantidade}</th>
-              <th>{"R$ " + valor}</th>
+
+              <th>
+                <input className="checkbox"
+                  type="checkbox"
+                  checked={concluido} 
+                  onChange={onToggle}
+                />
+              </th>
+              {}
+
+              <th className="texto">{nome}</th>
+              <th className="texto">{quantidade}</th>
+              <th className="texto">{"R$ " + valor}</th>
             </tr>
           </tbody>
         </table>
       </div>
       <p className="texto-data">{"Adicionado em " + data}</p>
     </li>
-)
+  );
 }
 
 export default ListItem;
